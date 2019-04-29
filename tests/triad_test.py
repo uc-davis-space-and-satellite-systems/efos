ce871fed9dbe90bb9111861aeb939bdf16eb734e
Vamsi <vamsieyunni@gmail.com>
2019-04-28 22:25:35 -0700

    Added triad and tests for triad

--- tests/triad_test.py
+++ tests/triad_test.py
@@ -1,1 +1,82 @@
+import sys
+sys.path.append("..")
+
 import numpy as np
+import triad
+
+if __name__ == "__main__":
+    # https://stackoverflow.com/questions/22222818/how-to-printing-numpy-array-with-3-decimal-places
+    np.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)})
+
+    acc_meas = np.array([0.0, 0.0, -1.0])
+    mag_meas = np.array([1.0, 0.0, 0.0])
+    acc_ref = np.array([0.0, 0.0, -1.0]) # constant reference vector
+    mag_ref = np.array([1.0, 0.0, 0.0]) # constant reference vector
+
+    print("Input Reference Vectors:")
+    print("Acc:", acc_meas)
+    print("Mag:", mag_meas)
+    print()
+
+    theta_roll = 0.0
+    theta_pitch = 45.0
+    theta_yaw = 0.0
+
+    print("Input Rotation:")
+    print("Roll:", theta_roll)
+    print("Pitch:", theta_pitch)
+    print("Yaw:", theta_yaw)
+    print()
+
+    theta_roll = np.radians(-1 * theta_roll)
+    theta_pitch = np.radians(-1 * theta_pitch)
+    theta_yaw = np.radians(-1 * theta_yaw)
+
+    rot_y = np.array([
+        [np.cos(theta_yaw), -1 * np.sin(theta_yaw), 0],
+        [np.sin(theta_yaw), np.cos(theta_yaw), 0],
+        [0, 0, 1]
+    ])
+
+    rot_p = np.array([
+        [np.cos(theta_pitch), 0, np.sin(theta_pitch)],
+        [0, 1, 0],
+        [-1 * np.sin(theta_pitch), 0, np.cos(theta_pitch)]
+    ])
+
+    rot_r = np.array([
+        [1, 0, 0],
+        [0, np.cos(theta_roll), -1 * np.sin(theta_roll)],
+        [0, np.sin(theta_roll), np.cos(theta_roll)]
+    ])
+
+    print("Rotation Matrix:")
+    print("R:")
+    print(rot_r)
+    print("P:")
+    print(rot_p)
+    print("Y:")
+    print(rot_y)
+
+    # being verbose for some reason helps wtf??
+    rot_tmp = np.dot(rot_r, rot_p)
+    rot = np.dot(rot_tmp, rot_y)
+    rot = rot.T
+
+    print("RPY:")
+    print(rot)
+    print()
+
+    acc_meas_rot = np.dot(rot, acc_meas)
+    mag_meas_rot = np.dot(rot, mag_meas)
+
+    print("Output Rotated Measurement Vectors:")
+    print("Acc:", acc_meas_rot)
+    print("Mag:", mag_meas_rot)
+    print()
+
+    rot_triad = triad.triad(acc_meas_rot, mag_meas_rot, acc_ref, mag_ref)
+
+    print("Output Rotations:")
+    print("Input:", np.degrees([-theta_roll, -theta_pitch, -theta_yaw]))
+    print("Output:", rot_triad)
--- triad.py
+++ triad.py
@@ -1,6 +1,47 @@
 import numpy as np
+import pysnooper
 
-def triad(acc_ref, acc_meas, mag_ref, mag_meas):
-    pass
+# @pysnooper.snoop(depth=1)
+def triad(acc_meas, mag_meas, acc_ref, mag_ref):
+    v_1b = np.asmatrix(acc_meas)    # Accelerometer vector in body frame (measurement)
+    v_2b = np.asmatrix(mag_meas)    # Magnetic Field vector in body frame (measurement)
+    v_1i = np.asmatrix(acc_ref)   # Accelerometer vector in reference frame
+    v_2i = np.asmatrix(mag_ref)    # Magnetic Field vector in reference frame
+
+    t_1b = v_1b                     # First basis vector in body frame
+    t_2b = np.cross(v_1b, v_2b)     # Second basis vector in body frame
+
+    if t_2b.all() != 0:
+        t_2b = np.divide(t_2b, np.linalg.norm(t_2b))    # Normalize second basis vector
+
+    t_3b = np.cross(t_1b, t_2b)     # Third basis vector in body frame
+
+    t_1i = v_1i                     # First basis vector in reference frame
+    t_2i = np.cross(v_1i, v_2i)     # Second basis vector in reference frame
+
+    if t_2i.all() != 0:
+        t_2i = np.divide(t_2i, np.linalg.norm(t_2i))    # Normalize second basis vector
+
+    t_3i = np.cross(t_1i, t_2i)     # Third basis vector in reference frame
+
+    R_bt = np.concatenate((t_1b, t_2b, t_3b)).T    # Construct DCM for body frame
+    R_it = np.concatenate((t_1i, t_2i, t_3i)).T    # Construct DCM for reference frame
+    R_bi = np.matmul(R_bt, R_it.T)      # Construct DCM from reference frame to body frame
+
+    theta_roll = np.arctan2(-1*R_bi.item((1,2)), R_bi.item((2,2)))      # Euler angle from x-axis
+    theta_pitch = np.arctan2(R_bi.item((0,2)), np.sqrt(np.power(R_bi.item((0,0)), 2) + np.power(R_bi.item((0,0)), 2)))  #Euler angle from y-axis
+    theta_yaw = np.arctan2(-1*R_bi.item((0,1)), R_bi.item((1,1)))   # Euler angle from z-axis
+
+    theta_roll = np.degrees(theta_roll)       # Convert Euler Angles from radians
+    theta_pitch = np.degrees(theta_pitch)     # to degrees
+    theta_yaw = np.degrees(theta_yaw)
+
+    return [theta_roll, theta_pitch, theta_yaw]
 
 if __name__ == "__main__":
+    acc_meas = np.matrix([0, 0, -1])    # accelerometer vector in body frame
+    mag_meas = np.matrix([1, 0, 0])    # Magnetic Field vector in body frame
+    acc_ref = np.matrix([0, 0, -1])    # accelerometer vector in reference frame
+    mag_ref = np.matrix([1, 0, 0])     # Magnetic Field vector in reference frame
+
+    print(triad(acc_meas, mag_meas, acc_ref, mag_ref))
