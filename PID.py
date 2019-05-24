bffd1b0d4d8b1a29bd8211c2ab98c10185000d7c
mridulsar <msarkar@ucdavis.edu>
2019-05-24 16:55:15 -0700

    PID trial
    take a look, please comment on any changes that should be made!

---
+++ PID.py
@@ -0,0 +1,84 @@
+##Referenced https://github.com/ivmech/ivPID/blob/master/PID.py and ht tps://en.wikipedia.org/wiki/PID_controller#Manual_tuning
+import time
+import matplotlib.pyplot as plt
+class PID: 
+    def __init__(self, current_yaw, set_yaw): ##P will need tuning, but a good starting point is 1 divided by the ‘encoder ticks per sample’ 
+        self.plotp = []
+        self.ploti = []
+        self.plotd = []
+        self.value = current_yaw
+        self.Kp = 0.2
+        self.Ki = 0.1 
+        self.Kd = 0.05
+        self.set = set_yaw##make this value about 75% of the ‘encoder ticks per sample’
+        self.settime = 1.0 ## can change this to create larger or smaller intervals
+        self.currenttime = time.time()
+        self.lasttime = self.currenttime
+
+        self.clear()
+
+    def clear(self): ##setting everything to zero..
+
+        self.Set = 0.0
+
+        self.PTerm = 0.0
+        self.ITerm = 0.0
+        self.DTerm = 0.0
+        self.preverror = 0.0
+        self.int_error = 0.0
+        self.windup_guard = 10. ##subject to change based on what we  
+        self.output = 0.0
+    
+    def update(self) : ##values from.. sun sensor.. reaction wheels... anything else? will have to modify this body of code to accomdate for more data points
+        ##Insert PID calculation in order to visualize how PID is working in unison with hardware
+        error = self.set - self.value
+        self.currenttime = time.time()
+        deltime = self.currenttime - self.lasttime #delta time!
+        delerror = error - self.preverror #delta error!
+
+        if deltime >= self.settime :
+            self.PTerm = self.Kp * error
+            self.ITerm += error * deltime 
+            if (self.ITerm < -self.windup_guard):
+                self.ITerm = -self.windup_guard
+            elif (self.ITerm > self.windup_guard):
+                self.ITerm = self.windup_guard
+            self.DTerm = 0.0
+            if deltime > 0 :
+                self.DTerm = delerror / deltime
+            
+            self.lasttime = self.currenttime
+            self.preverror = error
+
+            self.output = self.PTerm + (self.Ki * self.ITerm) + (self.Kd * self.DTerm)
+        ###PLOTTING PID
+        #p = self.plotp.append(self.PTerm)
+        #i = self.ploti.append(self.ITerm)
+        #d = self.plotd.append(self.DTerm)
+        #plt.plot((p,i,d,self.output),self.currenttime)
+        #plt.show
+        return self.output
+
+    ##def setKp(self, prop_gain) :
+        ##self.Kp = prop_gain
+    ##def setKi(self, int_gain) :
+        ##self.Ki = int_gain 
+    ##def setKd(self, der_gain) :
+        ##self.Kd = der_gain
+    def setWindup(self, windup):
+        """Integral windup, also known as integrator windup or reset windup,
+        refers to the situation in a PID feedback controller where
+        a large change in setpoint occurs (say a positive change)
+        and the integral terms accumulates a significant error
+        during the rise (windup), thus overshooting and continuing
+        to increase as this accumulated error is unwound
+        (offset by errors in the other direction).
+        The specific problem is the excess overshooting.
+        """
+        self.windup_guard = windup
+
+    def setsampleTime(self, sample_time):
+        self.sample_time = sample_time
+
+##Find other samples of code to decide if this option is the best! Think about a wrapper code to tie all these functions together THURSDAY
+##store values from each term into a list and write a function to graph these Values FRIDAY https://www.geeksforgeeks.org/graph-plotting-in-python-set-1/
---
+++ PIDWrapper.py
@@ -0,0 +1,15 @@
+from PID import PID
+set_yaw = 0
+current_yaw = 1
+pid = PID(set_yaw,current_yaw)
+while True:
+    #pid.setWindup(set_yaw) ##decide on what winup we should use, what is the maximal change that can occur in an ideal environment?
+    print(pid.update())
+while False:
+    pid.clear()
+##Start by modifying the KP constant and get the performance as good as you can before moving onto KD and then finally KI.
+##If the motor adjustments are too aggressive, swinging between too fast and too slow, reduce the constant.
+##   If the motor speed isn’t changing fast enough, increase the constant.
+##  Make any change in small increments; even a very small change can have a dramatic effect.
+
+
